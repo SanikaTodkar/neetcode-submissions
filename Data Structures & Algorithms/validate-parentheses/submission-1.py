0d317cc924b85cb8daa6c2b83f8e68208b9a72dc
Sanika <sanikatodkar1835@gmail.com>
2026-08-27 20:11:28 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        mappings = {")" : "(", "}": "{", "]":"["}
+
+        for char in s:
+            if char in mappings:
+                if not stack or stack[-1] != mappings[char]:
+                    return False
+
+                stack.pop()
+
+            else:
+                stack.append(char)
+
+        return not stack
